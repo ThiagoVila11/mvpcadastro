6c0a9fc9a84f42806f026451d077de2c8d8443d8
ThiagoVila11 <simbiox.thiago.tosatti@vila11.com.br>
2025-04-27 16:55:31 -0300

    ajuste dos settings.py

--- formulario_captura/formulario_captura/settings.py
+++ formulario_captura/formulario_captura/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-mm6xs&%0av^$$zoepp0)i^5t3xfyg2%5lb@fcod^3@i(^idid7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
