4585311f9558a6112af9e95da94f51bce3004416
ThiagoVila11 <simbiox.thiago.tosatti@vila11.com.br>
2025-05-05 08:32:49 -0300

    acertos nos formulários

--- formulario_captura/cadastro/forms.py
+++ formulario_captura/cadastro/forms.py
@@ -4,7 +4,11 @@ from .models import Cliente
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = '__all__' #['imagem', 'nome', 'cpf', 'score', 'email', 'telefone', 'data_nascimento',  'unidade', 'apto', 'observacoes']
+        fields = ['cpf', 'nome', 'score','profissao', 'estcivil', 'rgrne', 'email', 'telefone', 
+                  'endereco', 'data_nascimento', 'nomeresidente', 'cpfresidente', 'rgresidente', 
+                  'unidade', 'apto', 'nomeunidade', 'cnpjunidade', 'matriculaunidade', 'vagaunidade',
+                  'enderecounidade', 'nriptuunidade', 'vrunidade', 'iniciocontrato', 'prazocontrato',
+                  'terminocontrato', 'observacoes'] #'__all__' #['imagem', 'nome', 'cpf', 'score', 'email', 'telefone', 'data_nascimento',  'unidade', 'apto', 'observacoes']
         widgets = {
             'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
             'observacoes': forms.Textarea(attrs={'rows': 3}),
